a16e5b2eb8f2305b4b89dbf157fe80e91963ed8b
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-01-11 12:02:51 +0000

    initial commit. add background and asteroid

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import "./index.css"
+import Asteroid from "./components/Asteroid";
+
+export default function App() {
+
+  return (
+    <Asteroid/>
+
+  )
+
+}
---
+++ src/components/Asteroid.jsx
@@ -0,0 +1,13 @@
+import "../css/Asteroid.css"
+
+
+
+export default function Asteroid() {
+    return (
+        <div id="asteroid">
+            <img src="./src/assets/asteroid.png" alt="An image of a cartoon asteroid" />
+
+        </div>
+    )
+
+}
